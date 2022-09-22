00635ed33781fed9a281ce17994fb503ee315a39
Dilek ŞEKERCİ <89838610+dileksekerci@users.noreply.github.com>
2022-09-22 16:35:09 +0300

    starships component created

---
+++ src/components/Starships.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+
+function Starships() {
+
+    const [ships, setShips] = useState([]);
+    const [isloading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://swapi.dev/api/starships/")
+            .then((res) => res.json())
+            //.then((data) => setShips(data))
+            .then((data) => console.log(data))
+            .catch((e) => console.log(e))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    return (
+        <div>
+            <h1>Starships</h1>
+            {isloading && <div>Loading...</div>}
+
+            {/* {setShips.map((ship) => (
+                <div key={ship.id}>{ship.name}</div>
+            ))} */}
+        </div>
+    );
+}
+
+export default Starships;
