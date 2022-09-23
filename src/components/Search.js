e8b1e77a1d91b5846870bbc54ede564f343fcdb3
Dilek ŞEKERCİ <89838610+dileksekerci@users.noreply.github.com>
2022-09-23 19:44:48 +0300

    card update

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,13 @@
 import { useState } from "react";
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Search(ships) {
-    const [filterText, setFilterTExt] = useState('');
-
-    const filtered = ships.filter((item) => {
-        return Object.keys(item).some((key) =>
-            item[key]
-                .toString()
-                .toLowerCase()
-                .includes(filterText.toLocaleLowerCase())
-        );
-    });
+function Search() {
 
 
     return (
         <div>
-            <input placeholder="ship name or model" value={filterText} onchange={(e.target.value)} />
-
-            <ul>
-                {filtered.map((ship, i) => (
-                    <li key={i}>{ship.name}</li>
-                ))}
-            </ul>
+            <input placeholder="ship name or model" className="mb-5 mt-5" />
         </div>
     )
 }
