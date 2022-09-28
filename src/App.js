f31bd9ddd5302f05b6d733ef5c2ad5768753d91a
Dilek ŞEKERCİ <89838610+dileksekerci@users.noreply.github.com>
2022-09-29 02:45:21 +0300

    Added scroll button

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ import Search from './components/Search';
 export const Themecontext = createContext(null);
 
 function App() {
+  // dark-light mode
   const [theme, setTheme] = useState("light");
-
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   }
@@ -47,6 +47,13 @@ function App() {
               <Route path='*' element={<NoPage />} />
             </Routes>
           </div>
+          {/*scroll to top on button click*/}
+          <button className='topScroll' onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+          }}>
+            &#129153;
+          </button>
+
         </div>
       </Router >
     </Themecontext.Provider>
